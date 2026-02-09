c4a9869aaff200bd1708640cd3b930d64e9d7517
jamal khan <139732458+khanjamal59@users.noreply.github.com>
2026-02-09 22:57:27 +0530

    Update your_face_recognition.py

--- your_face_recognition.py
+++ your_face_recognition.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import csv
 import os
 
-# -------------------- ATTENDANCE FUNCTION --------------------
+#  ATTENDANCE FUNCTION --------------------
 
 def mark_attendance(name):
     with open("attendance.csv", "a", newline="") as f:
@@ -15,12 +15,12 @@ def mark_attendance(name):
         writer.writerow([name, date, time])
         print(f"[✔] Attendance marked for {name} at {time}")
 
-# -------------------- LOAD KNOWN FACES --------------------
+# LOADING KNOWN FACES --------------------
 
 KNOWN_DIR = "known_faces"
 
 if not os.path.exists(KNOWN_DIR):
-    print("❌ ERROR: known_faces folder not found!")
+    print(" ERROR: known_faces folder not found!")
     exit()
 
 print("[ℹ] Loading known faces...")
@@ -28,25 +28,25 @@ print("[ℹ] Loading known faces...")
 # DeepFace automatically uses them during matching
 known_faces_list = os.listdir(KNOWN_DIR)
 if len(known_faces_list) == 0:
-    print("❌ No images found in known_faces folder!")
+    print(" No images found in known_faces folder!")
     exit()
 
 print("[✔] Faces loaded:", known_faces_list)
 
-# -------------------- START CAMERA --------------------
+# STARTING THE CAMERA --------------------
 
 cap = cv2.VideoCapture(0)
-marked_today = set()    # prevent duplicate attendance in one session
+marked_today = set()    # preventing the duplicate attendance in one session
 
 print("[ℹ] System Started. Press 'Q' to quit.")
 
 while True:
     ret, frame = cap.read()
     if not ret:
-        print("❌ Camera Not Working!")
+        print(" Camera Not Working!")
         break
 
-    # Save frame temporarily (DeepFace requires file or array)
+    # Save frame temporarily (DeepFace requires file or array)-------------------------------
     cv2.imwrite("temp.jpg", frame)
 
     try:
@@ -68,7 +68,7 @@ while True:
             cv2.putText(frame, name, (30, 50),
                         cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
 
-            # Mark attendance only once per session
+            # Mark attendance only once per session---------------------------------
             if name not in marked_today:
                 mark_attendance(name)
                 marked_today.add(name)
@@ -80,12 +80,13 @@ while True:
     except Exception as e:
         print("Error:", e)
 
-    # Show camera
+    # Shows the camera---------------------------------------------------------------
     cv2.imshow("Face Recognition + Attendance", frame)
 
-    # Exit
+    # its for closing the camera excess-----------------------------------------------
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
 cap.release()
+
 cv2.destroyAllWindows()
